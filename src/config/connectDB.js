c30d6180a468c8ac2ac3a8a1c9659e3440574347
2602hoang <vuhuyhoangboj@gmail.com>
2024-07-09 02:04:46 +0700

    v1

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
-const sequelize = new Sequelize('bulbsbsafacvv5oilkfu', 'uzglsm7dg4xnna5y', 'mQDkBV5hR3iwtjJKJRCv' , {
-    host: 'bulbsbsafacvv5oilkfu-mysql.services.clever-cloud.com',
+const sequelize = new Sequelize('data', 'avnadmin', 'AVNS_ABeZwmOVjP8oaPaCDxB' , {
+    host: 'mysql-7048c9d-vuhuyhoanglol-2001.d.aivencloud.com',
+    port: 21062,
     dialect: 'mysql',
     dialectModule: mysql2,
     define: {
