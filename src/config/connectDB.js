920a97bd4e1942237ea2ba3e061a25f728ee48b6
2602hoang <vuhuyhoangboj@gmail.com>
2024-07-09 17:07:36 +0700

    fix

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,22 +1,22 @@
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
-const sequelize = new Sequelize('bulbsbsafacvv5oilkfu', 'uzglsm7dg4xnna5y', 'mQDkBV5hR3iwtjJKJRCv' , {
-    host: 'bulbsbsafacvv5oilkfu-mysql.services.clever-cloud.com',
-    // port: 21062,
+// const sequelize = new Sequelize('bulbsbsafacvv5oilkfu', 'uzglsm7dg4xnna5y', 'mQDkBV5hR3iwtjJKJRCv' , {
+//     host: 'bulbsbsafacvv5oilkfu-mysql.services.clever-cloud.com',
+//     // port: 21062,
+//     dialect: 'mysql',
+//     dialectModule: mysql2,
+//     define: {
+//     timestamps: false
+//   }
+
+  const sequelize = new Sequelize('data', 'avnadmin', 'AVNS_ABeZwmOVjP8oaPaCDxB' , {
+    host: 'mysql-7048c9d-vuhuyhoanglol-2001.d.aivencloud.com',
+    port: 21062,
     dialect: 'mysql',
     dialectModule: mysql2,
     define: {
     timestamps: false
   }
-
-  // const sequelize = new Sequelize('data', 'avnadmin', 'AVNS_ABeZwmOVjP8oaPaCDxB' , {
-  //   host: 'mysql-7048c9d-vuhuyhoanglol-2001.d.aivencloud.com',
-  //   port: 21062,
-  //   dialect: 'mysql',
-  //   dialectModule: mysql2,
-  //   define: {
-  //   timestamps: false
-  // }
   // DB_HOST = bulbsbsafacvv5oilkfu-mysql.services.clever-cloud.com
   // DB_NAME = bulbsbsafacvv5oilkfu
   // DB_USER = uzglsm7dg4xnna5y
