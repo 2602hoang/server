916c71b97e168f17ed59cb39c08ad2ed94198710
2602hoang <hoangvh@haibazo.com>
2025-01-15 13:46:32 +0700

    fix data login

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,167 +1,164 @@
-
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { Users } from '../module/index.js';
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Users } from "../module/index.js";
 
 const hashPassword = (password) => {
-
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 export async function loginUser(req, res) {
-    const { phone, password } = req.body;
+  const { phone, password } = req.body;
 
-    try {
-        // Validate input
-        if (!phone || !password) {
-            return res.status(400).json({
-                error: 3,
-                mes: 'Vui lòng điền đầy đủ thông tin'
-            });
+  try {
+    // Validate input
+    if (!phone || !password) {
+      return res.status(400).json({
+        error: 3,
+        mes: "Vui lòng điền đầy đủ thông tin",
+      });
+    }
+
+    // Function to find user and validate password
+    const loginUser1 = async ({ phone, password }) => {
+      try {
+        const user = await Users.findOne({ where: { phone }, raw: true });
+        if (!user) {
+          return {
+            error: 1,
+            mes: "Phone không tìm thấy",
+            access_token: null,
+          };
         }
 
-        // Function to find user and validate password
-        const loginUser1 = async ({ phone, password }) => {
-            try {
-                const user = await Users.findOne({ where: { phone }, raw: true });
-                if (!user) {
-                    return {
-                        error: 1,
-                        mes: 'Phone không tìm thấy',
-                        access_token: null
-                    };
-                }
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+          return {
+            error: 2,
+            mes: "Sai mật khẩu",
+            access_token: null,
+          };
+        }
 
-                const isValidPassword = await bcrypt.compare(password, user.password);
-                if (!isValidPassword) {
-                    return {
-                        error: 2,
-                        mes: 'Sai mật khẩu',
-                        access_token: null
-                    };
-                }
+        // Generate JWT token
+        const token = jwt.sign(
+          { id_user: user.id_user, phone: user.phone, id_role: user.id_role },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "2d" }
+        );
 
-                // Generate JWT token
-                const token = jwt.sign(
-                    { id_user: user.id_user, phone: user.phone , id_role: user.id_role },
-                    process.env.JWT_SECRET_KEY,
-                    { expiresIn: '2d' }
-                );
+        // Update user's remember_token in database
+        await Users.update(
+          { remember_token: token },
+          { where: { id_user: user.id_user } }
+        );
 
-                // Update user's remember_token in database
-                await Users.update({ remember_token: token }, { where: { id_user: user.id_user } });
-
-                return {
-                    error: 0,
-                    mes: 'Đăng nhập thành công',
-                    id_role : user.id_role,
-                    id_user : user.id_user,
-                    status : user.status,
-                    access_token: user.status === 0 ? token : null
-                };
-
-            } catch (error) {
-                throw new Error(error.message); // Rethrow the error for consistent handling
-            }
+        return {
+          error: 0,
+          mes: "Đăng nhập thành công",
+          username: user.username,
+          phone: user.phone,
+          avatar: user.avatar,
+          id_role: user.id_role,
+          id_user: user.id_user,
+          status: user.status,
+          access_token: user.status === 0 ? token : null,
         };
+      } catch (error) {
+        throw new Error(error.message); // Rethrow the error for consistent handling
+      }
+    };
 
-        // Call the login function
-        const loginResponse = await loginUser1({ phone, password });
-        return res.status(200).json(loginResponse);
-
-    } catch (error) {
-        return res.status(500).json({
-            error: 4,
-            mes: 'Đăng nhập thất bại',
-            details: error.message
-        });
-    }
+    // Call the login function
+    const loginResponse = await loginUser1({ phone, password });
+    return res.status(200).json(loginResponse);
+  } catch (error) {
+    return res.status(500).json({
+      error: 4,
+      mes: "Đăng nhập thất bại",
+      details: error.message,
+    });
+  }
 }
 
 export async function registerUser(req, res) {
-    const { phone, username, password } = req.body;
+  const { phone, username, password } = req.body;
 
-    try {
-        // Check if required fields are present
-        if (!phone || !username || !password) {
-            return res.status(400).json({
-                error: 1,
-                mes: 'Vui lòng nhập đầy đủ thông tin'
-            });
-        }
-
-    const registerUser1 = ({username, phone, password  }) => new Promise(async (resolve, reject) => {
-        try {
-            const [user, created] = await Users.findOrCreate({
-                where: { phone },
-                defaults: {
-                    
-                    username,
-                    phone,
-                    password: hashPassword(password),
-                    id_role : 125,
-                    
-                }
-            });
-
-            if (created) {
-                const token = jwt.sign(
-                    { id_user: user.id_user, phone: user.phone },
-                    process.env.JWT_SECRET_KEY,
-                    { expiresIn: '2d' }
-                );
-                //   user.remember_token = token;
-                //     await user.save();
-
-                resolve({
-                    error: 0,
-                    mes: 'Đăng ký thành công',
-                    token: token
-                });
-            } else {
-                resolve({
-                    error: 2,
-                    mes: 'Trùng số điện thoại',
-                    token: null
-                });
-            }
-        } catch (error) {
-            reject(error);
-        }
-    });
-
-
-        const result = await registerUser1({ phone, username, password });
-        return res.status(200).json(result);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 1, mes: 'Đăng ký thất bại ' });
+  try {
+    // Check if required fields are present
+    if (!phone || !username || !password) {
+      return res.status(400).json({
+        error: 1,
+        mes: "Vui lòng nhập đầy đủ thông tin",
+      });
     }
+
+    const registerUser1 = ({ username, phone, password }) =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const [user, created] = await Users.findOrCreate({
+            where: { phone },
+            defaults: {
+              username,
+              phone,
+              password: hashPassword(password),
+              id_role: 125,
+            },
+          });
+
+          if (created) {
+            const token = jwt.sign(
+              { id_user: user.id_user, phone: user.phone },
+              process.env.JWT_SECRET_KEY,
+              { expiresIn: "2d" }
+            );
+            //   user.remember_token = token;
+            //     await user.save();
+
+            resolve({
+              error: 0,
+              mes: "Đăng ký thành công",
+              token: token,
+            });
+          } else {
+            resolve({
+              error: 2,
+              mes: "Trùng số điện thoại",
+              token: null,
+            });
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    const result = await registerUser1({ phone, username, password });
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 1, mes: "Đăng ký thất bại " });
+  }
 }
 
 export async function updateUser(req, res) {
-    try {
-        const {username, avatar} = req.body;
-        const id_user = req.params.id_user;
-        console.log(id_user);
-        if(!id_user){
-            return res.status(400).json({
-                error: 1,
-                mes: 'khong tim thay user'
-            });
-        }
-            await Users.update({ username, avatar }, { where: { id_user } });
-            return res.status(200).json({
-                error: 0,
-                mes: 'cap nhat thanh cong'
-            });
-    } catch (error) {
-        return res.status(500).json({
-            error: 2,
-            mes: 'cap nhat that bai',
-            details: error.message
-        });
+  try {
+    const { username, avatar } = req.body;
+    const id_user = req.params.id_user;
+    console.log(id_user);
+    if (!id_user) {
+      return res.status(400).json({
+        error: 1,
+        mes: "khong tim thay user",
+      });
     }
-        
-             
+    await Users.update({ username, avatar }, { where: { id_user } });
+    return res.status(200).json({
+      error: 0,
+      mes: "cap nhat thanh cong",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: 2,
+      mes: "cap nhat that bai",
+      details: error.message,
+    });
+  }
 }
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,221 +1,258 @@
-
-import { cloudinary } from '../middlewear/cloudianary.config.js';
-import { Role, Users } from '../module/index.js';
-import bcrypt from 'bcryptjs';
-import { Op,  } from 'sequelize';
+import { cloudinary } from "../middlewear/cloudianary.config.js";
+import { Role, Users } from "../module/index.js";
+import bcrypt from "bcryptjs";
+import { Op } from "sequelize";
 const hashPassword = async (password) => {
-
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 const hashPassword1 = (password) => {
-
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 export async function getOne(req, res) {
-    try {
-        const { id_user } = req.params;
-        const parsedIdUser = parseInt(id_user);
-        if (isNaN(parsedIdUser)) {
-            return res.status(400).json({ success: false, message: 'Invalid user ID' });
-        }
-        const user = await Users.findOne({ where: { id_user: parsedIdUser } });
-       
-        return res.status(200).json({ success: true,  user });
-    } catch (error) {
-        throw new Error(error);
+  try {
+    const { id_user } = req.params;
+    const parsedIdUser = parseInt(id_user);
+    if (isNaN(parsedIdUser)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user ID" });
     }
+    const user = await Users.findOne({ where: { id_user: parsedIdUser } });
+
+    return res.status(200).json({ success: true, user });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export async function getAll(req, res) {
-    try {
-        const users = await Users.findAll({
-            order: [
-                ['id_role', 'ASC'],['status', 'ASC'] // Sắp xếp tăng dần theo id_role
-            ]
-        });
-        return res.status(200).json(users);
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const users = await Users.findAll({
+      order: [
+        ["id_role", "ASC"],
+        ["status", "ASC"], // Sắp xếp tăng dần theo id_role
+      ],
+    });
+    return res.status(200).json(users);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export async function addUser(req, res) {
-    try {
-        const { username, phone, password, id_role } = req.body;
-        const filedata = req.file; // Assuming this is handled by middleware (e.g., multer)
+  try {
+    const { username, phone, password, id_role } = req.body;
+    const filedata = req.file; // Assuming this is handled by middleware (e.g., multer)
 
-        const roleId = parseInt(id_role);
-        if (isNaN(roleId)) {
-            if (filedata) {
-                await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if role ID is invalid
-            }
-            return res.status(400).json({ success: false, message: 'Invalid role ID' });
-        }
-
-        const role = await Role.findByPk(roleId);
-        if (!role) {
-            if (filedata) {
-                await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if role ID is not found
-            }
-            return res.status(400).json({ success: false, message: 'Role not found' });
-        }
-
-        // Check if the phone number already exists
-        const whereCondition = phone ? { phone } : {};
-        const existingUser = await Users.findOne({ where: whereCondition });
-
-        if (existingUser) {
-            if (filedata) {
-                await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if phone number is not unique
-            }
-            return res.status(400).json({ success: false, message: 'Phone number already exists' });
-        }
-
-        const hashedPassword = hashPassword1(password);
-
-        const newUser = {
-            username,
-            phone,
-            password: hashedPassword,
-            avatar: filedata ? filedata.path : null, // Store file path or Cloudinary public ID
-            id_role: roleId,
-            status: 0
-        };
-
-        const user = await Users.create(newUser);
-
-        if (filedata) {
-            const cloudinaryResponse = await cloudinary.uploader.upload(filedata.path);
-            user.avatar = cloudinaryResponse.secure_url; // Update user's avatar field with Cloudinary URL
-            await user.save(); // Save the updated user data with Cloudinary URL
-        }
-
-        return res.status(200).json({ success: true, data: user });
-
-    } catch (error) {
-        console.error('Error adding user:', error);
-
-        if (req.file) {
-            await cloudinary.uploader.destroy(req.file.filename); // Clean up uploaded file if an error occurs during insertion
-        }
-
-        return res.status(500).json({ success: false, message: error.message });
+    const roleId = parseInt(id_role);
+    if (isNaN(roleId)) {
+      if (filedata) {
+        await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if role ID is invalid
+      }
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid role ID" });
     }
+
+    const role = await Role.findByPk(roleId);
+    if (!role) {
+      if (filedata) {
+        await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if role ID is not found
+      }
+      return res
+        .status(400)
+        .json({ success: false, message: "Role not found" });
+    }
+
+    // Check if the phone number already exists
+    const whereCondition = phone ? { phone } : {};
+    const existingUser = await Users.findOne({ where: whereCondition });
+
+    if (existingUser) {
+      if (filedata) {
+        await cloudinary.uploader.destroy(filedata.filename); // Clean up uploaded file if phone number is not unique
+      }
+      return res
+        .status(400)
+        .json({ success: false, message: "Phone number already exists" });
+    }
+
+    const hashedPassword = hashPassword1(password);
+
+    const newUser = {
+      username,
+      phone,
+      password: hashedPassword,
+      avatar: filedata ? filedata.path : null, // Store file path or Cloudinary public ID
+      id_role: roleId,
+      status: 0,
+    };
+
+    const user = await Users.create(newUser);
+
+    if (filedata) {
+      const cloudinaryResponse = await cloudinary.uploader.upload(
+        filedata.path
+      );
+      user.avatar = cloudinaryResponse.secure_url; // Update user's avatar field with Cloudinary URL
+      await user.save(); // Save the updated user data with Cloudinary URL
+    }
+
+    return res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    console.error("Error adding user:", error);
+
+    if (req.file) {
+      await cloudinary.uploader.destroy(req.file.filename); // Clean up uploaded file if an error occurs during insertion
+    }
+
+    return res.status(500).json({ success: false, message: error.message });
+  }
 }
 
 export async function deleteUser(req, res) {
-    try {
-        const { id_user } = req.params;
-        const parsedIdUser = parseInt(id_user);
-        if (isNaN(parsedIdUser)) {
-            return res.status(400).json({ success: false, message: 'Invalid user ID' });
-        }
-        const user = await Users.findOne({ where: { id_user: parsedIdUser } });
-        if (!user) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
-        
-        // Update the user's status to 1 instead of deleting
-        await user.update({ status: 1 });
-
-        return res.status(200).json({ success: true, message: 'User status updated to 1 successfully' });
-    } catch (error) {
-        console.error('Error updating user status:', error);
-        return res.status(500).json({ success: false, message: 'Internal server error' });
+  try {
+    const { id_user } = req.params;
+    const parsedIdUser = parseInt(id_user);
+    if (isNaN(parsedIdUser)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user ID" });
     }
+    const user = await Users.findOne({ where: { id_user: parsedIdUser } });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Update the user's status to 1 instead of deleting
+    await user.update({ status: 1 });
+
+    return res.status(200).json({
+      success: true,
+      message: "User status updated to 1 successfully",
+    });
+  } catch (error) {
+    console.error("Error updating user status:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 }
 export async function deleteUser1(req, res) {
-    try {
-        const { id_user } = req.params;
-        const parsedIdUser = parseInt(id_user);
-        if (isNaN(parsedIdUser)) {
-            return res.status(400).json({ success: false, message: 'Invalid user ID' });
-        }
-        const user = await Users.findOne({ where: { id_user: parsedIdUser } });
-        if (!user) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
-        
-        // Update the user's status to 1 instead of deleting
-        await user.update({ status: 0 });
-
-        return res.status(200).json({ success: true, message: 'User status updated to 1 successfully' });
-    } catch (error) {
-        console.error('Error updating user status:', error);
-        return res.status(500).json({ success: false, message: 'Internal server error' });
+  try {
+    const { id_user } = req.params;
+    const parsedIdUser = parseInt(id_user);
+    if (isNaN(parsedIdUser)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user ID" });
     }
+    const user = await Users.findOne({ where: { id_user: parsedIdUser } });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Update the user's status to 1 instead of deleting
+    await user.update({ status: 0 });
+
+    return res.status(200).json({
+      success: true,
+      message: "User status updated to 1 successfully",
+    });
+  } catch (error) {
+    console.error("Error updating user status:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 }
 
 export async function updateUser(req, res) {
-    try {
-        const { username, phone, password, id_role } = req.body;
-        const { id_user } = req.params;
-        const parsedIdUser = parseInt(id_user);
+  try {
+    const { username, phone, password, id_role } = req.body;
+    const { id_user } = req.params;
+    const parsedIdUser = parseInt(id_user);
 
-        if (isNaN(parsedIdUser)) {
-            return res.status(400).json({ success: false, message: 'Invalid user ID' });
-        }
-
-        const user = await Users.findByPk(parsedIdUser);
-        if (!user) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
-
-        // Check if the phone number already exists for another user
-        const existingUserWithPhone = await Users.findOne({ where: { phone } });
-        if (existingUserWithPhone && existingUserWithPhone.id_user !== parsedIdUser) {
-            return res.status(409).json({ success: false, message: 'Phone number already exists' });
-        }
-
-        let hashedPassword;
-        if (password) {
-            hashedPassword = await hashPassword(password); // Hash the new password
-        } else {
-            hashedPassword = user.password; // Keep the existing password
-        }
-
-        const updateData = {
-            username,
-            phone,
-            password: hashedPassword,
-            id_role
-        };
-
-
-        // Check if there's a new avatar file to upload
-        if (req.file) {
-            // Upload new avatar to Cloudinary
-            const result = await cloudinary.uploader.upload(req.file.path);
-
-            // Delete old avatar from Cloudinary if it exists
-            if (user.avatar_public_id) {
-                await cloudinary.uploader.destroy(user.avatar_public_id);
-            }
-
-            updateData.avatar = result.secure_url;
-            updateData.avatar_public_id = result.public_id;
-        }
-
-        await user.update(updateData);
-        return res.status(200).json({ success: true, message: 'User updated successfully' });
-    } catch (error) {
-        console.error('Error updating user:', error.message);
-        return res.status(500).json({ success: false, message: 'An error occurred while updating the user' });
+    if (isNaN(parsedIdUser)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user ID" });
     }
+
+    const user = await Users.findByPk(parsedIdUser);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Check if the phone number already exists for another user
+    const existingUserWithPhone = await Users.findOne({ where: { phone } });
+    if (
+      existingUserWithPhone &&
+      existingUserWithPhone.id_user !== parsedIdUser
+    ) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Phone number already exists" });
+    }
+
+    let hashedPassword;
+    if (password) {
+      hashedPassword = await hashPassword(password); // Hash the new password
+    } else {
+      hashedPassword = user.password; // Keep the existing password
+    }
+
+    const updateData = {
+      username,
+      phone,
+      password: hashedPassword,
+      id_role,
+    };
+
+    // Check if there's a new avatar file to upload
+    if (req.file) {
+      // Upload new avatar to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      // Delete old avatar from Cloudinary if it exists
+      if (user.avatar_public_id) {
+        await cloudinary.uploader.destroy(user.avatar_public_id);
+      }
+
+      updateData.avatar = result.secure_url;
+      updateData.avatar_public_id = result.public_id;
+    }
+
+    await user.update(updateData);
+    return res
+      .status(200)
+      .json({ success: true, message: "User updated successfully" });
+  } catch (error) {
+    console.error("Error updating user:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while updating the user",
+    });
+  }
 }
 export async function getAllRole(req, res) {
-    try {
-        const roles = await Role.findAll({
-            attributes: ['id_role', 'name'],
-            where: {
-                id_role: {
-                    [Op.in]: [124,125]  // Include only roles with id_role 124 or 125
-                }
-            }
-        });
-        return res.status(200).json(roles);
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const roles = await Role.findAll({
+      attributes: ["id_role", "name"],
+      where: {
+        id_role: {
+          [Op.in]: [124, 125], // Include only roles with id_role 124 or 125
+        },
+      },
+    });
+    return res.status(200).json(roles);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
-
